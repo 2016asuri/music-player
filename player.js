0b141e9f8cf9e50a16694056d1501a255fff402f
2016asuri <ananyas@andrew.cmu.edu>
2018-07-21 17:23:53 -0400

    add youtube player

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 var app = angular.module('musicApp', []);
 app.controller("musicCtrl", function($scope) {
-    $scope.products = [{name: 'Song1', platform: 'YouTube', link:'0000'}, 
+    $scope.products = [{name: 'Song1', platform: 'YouTube', link:'bWwBJqVYSZI'}, 
     				   {name: 'Song2', platform: 'Google Drive', link:'0001'}, 
     				   {name: 'Song3', platform: 'Spotify', link:'0002'}];
     $scope.addItem = function () {
@@ -21,8 +21,65 @@ app.controller("musicCtrl", function($scope) {
     $scope.playSong = function (x) {
         $scope.errortext = "";
         song_to_play = $scope.products[x]
-        alert(song_to_play.name)
+        if(song_to_play.platform == 'YouTube'){
+        	videoId = song_to_play.link;
+        	load()   
+        }
+             
     } 
+
+    var videoId;
+	function load()
+	{
+		if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+		   var tag = document.createElement('script');
+    		tag.src = "https://www.youtube.com/iframe_api";
+    		var firstScriptTag = document.getElementsByTagName('script')[0];
+    		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    		window.onYouTubePlayerAPIReady = function() {
+      		onYouTubePlayer();
+    	};
+
+  	} else {
+
+    	onYouTubePlayer();
+
+  		}
+	}
+	var player;
+	function onYouTubePlayer() 
+	{
+		player = new YT.Player('player', 
+		{
+		height: '390',
+		width: '640',
+		videoId: videoId,
+		events: 
+		{
+		  'onReady': onPlayerReady,
+		  'onStateChange': onPlayerStateChange
+		},
+		host: 'https://www.youtube.com'
+		});
+	} 
+	function onPlayerReady(event) 
+	{
+		event.target.playVideo();
+	}
+	var done = false;
+	function onPlayerStateChange(event) 
+	{
+		if (event.data == YT.PlayerState.PLAYING && !done) 
+		{
+		  	setTimeout(stopVideo, (player.getDuration()-1)*1000);
+		  	done = true;
+		}
+	}
+	function stopVideo() 
+	{
+		player.stopVideo();
+	}    
 });
 
 
