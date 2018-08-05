b661c26ab1e86a4c32faf83657b2ac7ea17f9705
2016asuri <ananyas@andrew.cmu.edu>
2018-08-05 12:29:18 -0400

    fix login

--- php/login.php
+++ php/login.php
@@ -1,36 +1,4 @@
 <?php
- 
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-/*define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'user1');
-define('DB_PASSWORD', 'p@ssw0rd');
-define('DB_NAME', 'music_player_db');
- 
-$DBcon; 
-//Connect with database
- try
- {
-     $DBcon = new PDO("mysql:host={DB_SERVER};dbname={DB_NAME}",DB_USERNAME,DB_PASSWORD);
-     $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- }
- catch(PDOException $e)
- {
-     echo "ERROR : ".$e->getMessage();
- }
- 
- 
-// verifying user from database using PDO
-$stmt = $DBcon->prepare("SELECT username, password from users WHERE username='".$_POST['username']."' && password='".$_POST['password']."'");
-$stmt->execute();
-$row = $stmt->rowCount();
-if ($row > 0){
-    echo "correct";
-} else{
-    echo 'wrong';
-}*/
-
-
 header("Access-Control-Allow-Origin: *");
 header("charset=utf-8");
 
@@ -40,16 +8,19 @@ $password = "p@ssw0rd";
 $database = "music_player_db";
 
 $conn = new mysqli($servername, $username, $password, $database);
-$sql = "DESCRIBE users;";
-//$sql = "SELECT id,username,password from users;";
-//$sql = "SELECT id,username, password from users WHERE username='".$_POST['username']."' && password='".$_POST['password']."';";
-//echo $sql;
+
+$sql = "SELECT id,username, password from users WHERE username='".$_POST['username']."' && password='".$_POST['password']."';";
 $results = mysqli_query($conn, $sql);
-//$json = json_encode($results);
-/*if ($json->rowCount > 0){
-    echo "correct";
-} else{
-    echo 'wrong';
-}*/
-echo $results;
+$data = array();
+
+while($row = mysqli_fetch_assoc($results)) {
+    $data[] = $row;
+}
+
+if(count($data)>0){
+	echo "correct";
+}
+else {
+	echo "wrong";
+}
 ?>
